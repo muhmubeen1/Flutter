10be9a0883e0786eab3b57f13dfeed375b634cbe
muhmubeen1 <muhmubeen1@gmail.com>
2024-08-07 15:21:34 +0500

    create database

---
+++ Cart_with_SQL/cart/lib/db_helper.dart
@@ -0,0 +1,38 @@
+import 'dart:io' as io;
+
+import 'package:cart/cart_model.dart';
+import "package:path_provider/path_provider.dart";
+import 'package:sqflite_common/sqflite.dart';
+
+class DbHelper {
+  static Database? _db;
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db!;
+    }
+    _db = await initDb();
+    return _db!;
+  }
+
+  initDb() async {
+    io.Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = '${documentDirectory.path}cart.db';
+    var db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+    return db;
+  }
+
+  _onCreate(Database db, int version) async {
+    await db.execute(
+        'CREATE TABLE cart (id INTEGER PRIMARY KEY , productId VARCHAR UNIQUE,productName TEXT,initialPrice INTEGER, productPrice INTEGER , quantity INTEGER, unitTag TEXT , image TEXT )');
+  }
+
+  Future<Cart> insert(Cart cart) async {
+    var dbClient = await db;
+    await dbClient.insert('cart', cart.toMap());
+    return cart;
+  }
+}
