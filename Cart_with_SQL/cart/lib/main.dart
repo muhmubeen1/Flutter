2fcd5b3987c1af2da82ca270e7b117619dc0253e
muhmubeen1 <muhmubeen1@gmail.com>
2024-08-07 16:46:10 +0500

    build with provider

--- Cart_with_SQL/cart/lib/main.dart
+++ Cart_with_SQL/cart/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:cart/cart_provider.dart';
 import 'package:cart/products.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +13,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Products(),
+    return ChangeNotifierProvider(
+      create: (_) => CartProvider(),
+      child: Builder(builder: (BuildContext context) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: Products(),
+        );
+      }),
     );
   }
 }
