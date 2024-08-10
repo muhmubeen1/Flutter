fda2f16abdc178e110a45088935b47c2a880584a
muhmubeen1 <muhmubeen1@gmail.com>
2024-08-10 21:47:36 +0500

    add delete method

--- Cart_with_SQL/cart/lib/db_helper.dart
+++ Cart_with_SQL/cart/lib/db_helper.dart
@@ -2,11 +2,12 @@ import 'dart:io' as io;
 
 import 'package:cart/cart_model.dart';
 import 'package:path/path.dart';
-import "package:path_provider/path_provider.dart";
-import 'package:sqflite_common/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart'; // Corrected import
 
 class DbHelper {
   static Database? _db;
+
   Future<Database> get db async {
     if (_db != null) {
       return _db!;
@@ -15,7 +16,7 @@ class DbHelper {
     return _db!;
   }
 
-  initDb() async {
+  Future<Database> initDb() async {
     io.Directory documentDirectory = await getApplicationDocumentsDirectory();
     String path = join(documentDirectory.path, 'cart.db');
     var db = await openDatabase(
@@ -26,14 +27,25 @@ class DbHelper {
     return db;
   }
 
-  _onCreate(Database db, int version) async {
+  void _onCreate(Database db, int version) async {
     await db.execute(
-        'CREATE TABLE cart (id INTEGER PRIMARY KEY , productId VARCHAR UNIQUE,productName TEXT,initialPrice INTEGER, productPrice INTEGER , quantity INTEGER, unitTag TEXT , image TEXT )');
+      'CREATE TABLE cart (id INTEGER PRIMARY KEY , productId VARCHAR UNIQUE,productName TEXT,initialPrice INTEGER, productPrice INTEGER , quantity INTEGER, unitTag TEXT , image TEXT )',
+    );
   }
 
   Future<Cart> insert(Cart cart) async {
     var dbClient = await db;
-    await dbClient.insert('cart', cart.toMap());
+    try {
+      await dbClient.insert('cart', cart.toMap());
+    } catch (e) {
+      print("Error inserting cart item: $e");
+      // Handle specific exceptions as needed
+    }
     return cart;
+  }
+
+  Future closeDb() async {
+    var dbClient = await db;
+    await dbClient.close();
   }
 }
