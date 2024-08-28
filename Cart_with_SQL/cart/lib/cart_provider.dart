9d1629e786c3ad67220f4ba2e509d0b4dc9e01bd
muhmubeen1 <muhmubeen1@gmail.com>
2024-08-28 23:00:07 +0500

    Intialize

--- Cart_with_SQL/cart/lib/cart_provider.dart
+++ Cart_with_SQL/cart/lib/cart_provider.dart
@@ -1,11 +1,18 @@
+import 'package:cart/db_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'cart_model.dart';
+
 class CartProvider with ChangeNotifier {
+  DbHelper db = DbHelper();
   int _counter = 0;
   int get counter => _counter;
   double _totalPrice = 0.0;
   double get totalPrice => _totalPrice;
+
+  late Future<List<Cart>> _cart;
+  Future<List<Cart>> get cart => _cart;
 
   void _setPrefItem() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
---
+++ Flutter_Provider/flutter_with_provider/android/app/src/main/kotlin/com/example/flutter_with_provider/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_with_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
