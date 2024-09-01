58f6c5181fb017baaaebd0d1db7ea34c99bf78c4
muhmubeen1 <muhmubeen1@gmail.com>
2024-09-02 00:32:57 +0500

    Add the multiple provider screen

--- Flutter_Provider/project/lib/screen/example_one.dart
+++ Flutter_Provider/project/lib/screen/example_one.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:project/provider/example_oneProvider.dart';
+import 'package:provider/provider.dart';
 
 class ExampleOne extends StatefulWidget {
   const ExampleOne({Key? key}) : super(key: key);
@@ -7,40 +9,49 @@ class ExampleOne extends StatefulWidget {
 }
 
 class _ExampleOneState extends State<ExampleOne> {
-  double value = 1.0;
+  // double value = 1.0;
+
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ExampleOneprovider>(context, listen: false);
     return MaterialApp(
       home: Scaffold(
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Slider(
-                min: 0,
-                max: 1,
-                value: value,
-                onChanged: (val) {
-                  value = val;
-                  setState(() {});
-                }),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    height: 100,
-                    color: Colors.yellow.withOpacity(value),
-                    child: Center(child: Text("Conatiner 1")),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    height: 100,
-                    color: Colors.green.withOpacity(value),
-                    child: Center(child: Text("Conatiner 2")),
-                  ),
-                ),
-              ],
+            Consumer<ExampleOneprovider>(
+              builder: (context, value, child) {
+                return Slider(
+                    min: 0,
+                    max: 1,
+                    value: provider.value,
+                    onChanged: (val) {
+                      provider.setValue(val);
+                    });
+              },
+            ),
+            Consumer<ExampleOneprovider>(
+              builder: (context, value, child) {
+                return Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 100,
+                        color: Colors.yellow.withOpacity(provider.value),
+                        child: Center(child: Text("Conatiner 1")),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 100,
+                        color: Colors.green.withOpacity(provider.value),
+                        child: Center(child: Text("Conatiner 2")),
+                      ),
+                    ),
+                  ],
+                );
+              },
             )
           ],
         ),
