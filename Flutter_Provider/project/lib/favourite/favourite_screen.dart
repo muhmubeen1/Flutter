9da137f983b58df67d8bab5cc73ea3a506d8a37c
muhmubeen1 <muhmubeen1@gmail.com>
2024-09-08 00:33:27 +0500

    Complete Favourite Screen

--- Flutter_Provider/project/lib/favourite/favourite_screen.dart
+++ Flutter_Provider/project/lib/favourite/favourite_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:project/favourite/myfavourite.dart';
+import 'package:project/provider/favourite_provider.dart';
+import 'package:provider/provider.dart';
 
 class FavouriteScreen extends StatefulWidget {
   const FavouriteScreen({Key? key}) : super(key: key);
@@ -9,9 +12,51 @@ class FavouriteScreen extends StatefulWidget {
 class _FavouriteScreenState extends State<FavouriteScreen> {
   @override
   Widget build(BuildContext context) {
+    final favourite = Provider.of<FavouriteProvider>(context);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: SafeArea(child: ,),
+        appBar: AppBar(
+          actions: [
+            InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyFavourite()));
+                },
+                child: Icon(Icons.favorite)),
+          ],
+          title: const Text("Favourite Screen"),
+          backgroundColor: Colors.yellow,
+        ),
+        body: SafeArea(
+            child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 100,
+                  itemBuilder: (context, index) {
+                    return Consumer<FavouriteProvider>(
+                      builder: (context, value, child) {
+                        return ListTile(
+                          onTap: () {
+                            if (value.selectItems.contains(index)) {
+                              value.removeItems(index);
+                            } else {
+                              value.addItem(index);
+                            }
+                          },
+                          title: Text("Stella"),
+                          trailing: favourite.selectItems.contains(index)
+                              ? Icon(Icons.favorite)
+                              : Icon(Icons.favorite_border_outlined),
+                          subtitle: Text(index.toString()),
+                        );
+                      },
+                    );
+                  }),
+            )
+          ],
+        )),
       ),
     );
   }
